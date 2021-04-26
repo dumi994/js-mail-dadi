f9fab00f49a76208fd75c1c3587a2468e5525856
dumi994 <dumi.9944@gmail.com>
2021-04-27 00:05:46 +0200

    chiedi email

---
+++ mail/main.js
@@ -0,0 +1,10 @@
+//Chiedi all’utente la sua email, 
+var mailPresenti = ['dumi.9944@gmail.com', 'marione@gmail.com', 'pinosilvestre@gmail.com'];
+var mail = prompt('Inserisci qui la tua email');
+console.log(mail);
+
+/* 
+
+// controlla che sia nella lista di chi può accedere,
+
+//stampa un messaggio appropriato sull’esito del controllo.
