ee355f9a443ee44cde8046232dbe20caaabfea8a
dumi994 <dumi.9944@gmail.com>
2021-04-27 10:05:40 +0200

    inserito ciclo

--- mail/main.js
+++ mail/main.js
@@ -1,10 +1,26 @@
 //Chiedi all’utente la sua email, 
-var mailPresenti = ['dumi.9944@gmail.com', 'marione@gmail.com', 'pinosilvestre@gmail.com'];
-var mail = prompt('Inserisci qui la tua email');
-console.log(mail);
+var userEmail = prompt('Inserisci qui la tua email');
+console.log(userEmail);
+var mailList = ['dumi.9944@gmail.com', 'marione@gmail.com', 'pinosilvestre@gmail.com'];
+var invitato = false
+var message;
 
-/* 
+ for (var i = 0; i < mailList.length; i++){
+    var email = mailList[i];
+    if (email == userEmail){   //mailList[0] = dumi.9944@gmail.com
+        invitato = true;
+    }
+ }
+
 
 // controlla che sia nella lista di chi può accedere,
+console.log(invitato);
 
+if (invitato == true){
+    message = 'benvenuto alla festa'
+}else{
+    message = 'sorry, non puoi entrare'
+}
 //stampa un messaggio appropriato sull’esito del controllo.
+
+document.getElementById("titolo").innerHTML = message;
